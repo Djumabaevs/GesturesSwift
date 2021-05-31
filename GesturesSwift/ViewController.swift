b2592ef587aca9fcd4496d9833cd8be271547a6a
BakytDzhumabev <djumabaevb@gmail.com>
2021-05-31 10:40:51 +0600

    ui set up

--- GesturesSwift/ViewController.swift
+++ GesturesSwift/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
