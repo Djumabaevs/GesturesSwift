9e87e4d31a63555a6d4772837483f8a408aeda54
BakytDzhumabev <djumabaevb@gmail.com>
2021-05-31 11:00:06 +0600

    done

--- GesturesSwift/ViewController.swift
+++ GesturesSwift/ViewController.swift
@@ -11,15 +11,29 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var myLabel: UILabel!
     @IBOutlet weak var myImage: UIImageView!
+    var isFerrari = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         myImage.isUserInteractionEnabled = true
         
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        myImage.addGestureRecognizer(gestureRecognizer)
     }
     
     @objc func changePic() {
+      
+        if isFerrari == true {
+            myImage.image = UIImage(named: "bugatti")
+            myLabel.text = "Bugatti Veyron"
+            isFerrari = false
+        } else {
+            myImage.image = UIImage(named: "ferrari")
+            myLabel.text = "Ferrari"
+            isFerrari = true
+        }
+        
         
     }
 
