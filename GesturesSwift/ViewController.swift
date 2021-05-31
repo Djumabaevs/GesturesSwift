85e6d3a83edd996cc97c7084f651f2064481dfb9
BakytDzhumabev <djumabaevb@gmail.com>
2021-05-31 21:06:23 +0600

    add background timer

--- GesturesSwift/ViewController.swift
+++ GesturesSwift/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var myImage: UIImageView!
     var isFerrari = true
     
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,7 +25,32 @@ class ViewController: UIViewController {
         
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
         myImage.addGestureRecognizer(gestureRecognizer)
+        
+        counter = 10
+        timerLabel.text = "Timer: \(counter)"
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
+        
+      
     }
+    
+    @objc func timerFunction() {
+        timerLabel.text = "Timer: \(counter)"
+        counter -= 1
+        
+        if counter == 0 {
+            timer.invalidate()
+            timerLabel.text = "Time's over!"
+        }
+    }
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//        counter = 10
+//        for time in 1...10 {
+//            counter -= counter
+//            timerLabel.text = "Time:  \(counter)"
+//            Thread.sleep(forTimeInterval: 1)
+//        }
+//    }
     
     @objc func changePic() {
       
